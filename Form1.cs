a78b4ef486f95952a5df45170a2538506e2c91d4
Carl-Joseph-Alapide <carljosephalapide@gmail.com>
2022-06-24 14:55:34 +0800

    changed the sorting function to shorten the codes

--- Form1.cs
+++ Form1.cs
@@ -11,7 +11,7 @@ namespace Contact_Tracing_App
 
         private void ContactTracing_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Trace it","Welcome");
+            MessageBox.Show("Trace it", "Welcome");
         }
         private void btnsubmit_Click(object sender, EventArgs e)
         {
@@ -25,7 +25,7 @@ namespace Contact_Tracing_App
                 all = false;
             else if (string.IsNullOrEmpty(txtbxtime.Text))
                 all = false;
-            else if (cmboxmonth.Text=="select month")
+            else if (cmboxmonth.Text == "select month")
                 all = false;
             else if (cmboxDay.Text == "Day")
                 all = false;
@@ -40,11 +40,11 @@ namespace Contact_Tracing_App
                 all = false;
             else if (string.IsNullOrEmpty(txtbxAge.Text))
                 all = false;
-            else if ((!chkbxyes1.Checked)&&(!chkbxNo1.Checked))
+            else if ((!chkbxyes1.Checked) && (!chkbxNo1.Checked))
                 all = false;
             else if ((!chkbxyes2.Checked) && (!chkbxNo2.Checked))
                 all = false;
-            if (all)           
+            if (all)
             {
                 MessageBox.Show("Remember to Always stay safe", "Thank you");
             }
@@ -144,156 +144,96 @@ namespace Contact_Tracing_App
                 chkbxNo2.Checked = false;
             }
         }
-
         private void btnlist_Click(object sender, EventArgs e)
         {
             StreamReader Visitors = new StreamReader(@"C:\Users\Carl Joseph\source\repos\Contact Tracing App\Responses\Responses.txt");
-            MessageBox.Show (Visitors.ReadToEnd());
+            MessageBox.Show(Visitors.ReadToEnd());
         }
-
+       
+        string month = "Months";
         private void File()
         {
             //making a copy of responses
             if (cmboxmonth.Text == "January")
             {
-                StreamWriter responses = new StreamWriter(@"C:\Users\Carl Joseph\source\repos\Contact Tracing App\Responses\Januarry Responses.txt", true);
-                ++counter;
-                responses.WriteLine("Respondent " + counter);
-                //Personal Information Part
-                responses.WriteLine("Name: " + txtbxFirstName.Text + " " + txtbxMiddleName.Text + " " + txtbxLastName.Text);
-                responses.WriteLine("Age: " + txtbxAge.Text);
-                responses.WriteLine("Contact no.: " + txtbxcontactno.Text);
-                responses.WriteLine("Email Address: " + txtbxEmail.Text);
-                responses.WriteLine("Address: " + txtbxaddress.Text);
-                responses.WriteLine("Time of Visit: " + txtbxtime.Text);
-                responses.WriteLine("Date of Visit: " + cmboxmonth.Text + " " + cmboxDay.Text + ", " + cmboxYear.Text);
-                //Health Check question 1
-                if (chkbxSymptom1.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom1.Text);
-                }
-                if (chkbxSymptom2.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom2.Text);
-                }
-                if (chkbxSymptom3.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom3.Text);
-                }
-                if (chkbxSymptom4.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom4.Text);
-                }
-                if (chkbxSymptom5.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom5.Text);
-                }
-                if (chkbxSymptom6.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom6.Text);
-                }
-                if (chkbxSymptom7.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom7.Text);
-                }
-                if (chkbxSymptom8.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom8.Text);
-                }
-                if (chkbxSymptom9.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom9.Text);
-                }
-                //Health Check Question 2
-                if (chkbxyes1.Checked)
-                {
-                    responses.WriteLine("Have been in close contact with a probable or confirmed case of COVID-19");
-                }
-                else
-                {
-                    responses.WriteLine("Did not have a close contact with a probable or confirmed case of COVID-19");
-                }
-                //Health Check Question 3
-                if (chkbxyes2.Checked)
-                {
-                    responses.WriteLine("Fully Vaccinated");
-                }
-                else
-                {
-                    responses.WriteLine("Not Fully Vaccinated");
-                }
-                responses.Close();
+                month = "January";
+                File_content();
             }
             else if (cmboxmonth.Text == "February")
             {
-                StreamWriter responses = new StreamWriter(@"C:\Users\Carl Joseph\source\repos\Contact Tracing App\Responses\February Responses.txt", true);
-                ++counter;
-                responses.WriteLine("Respondent " + counter);
-                //Personal Information Part
-                responses.WriteLine("Name: " + txtbxFirstName.Text + " " + txtbxMiddleName.Text + " " + txtbxLastName.Text);
-                responses.WriteLine("Age: " + txtbxAge.Text);
-                responses.WriteLine("Contact no.: " + txtbxcontactno.Text);
-                responses.WriteLine("Email Address: " + txtbxEmail.Text);
-                responses.WriteLine("Address: " + txtbxaddress.Text);
-                responses.WriteLine("Time of Visit: " + txtbxtime.Text);
-                responses.WriteLine("Date of Visit: " + cmboxmonth.Text + " " + cmboxDay.Text + ", " + cmboxYear.Text);
-                //Health Check question 1
-                if (chkbxSymptom1.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom1.Text);
-                }
-                if (chkbxSymptom2.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom2.Text);
-                }
-                if (chkbxSymptom3.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom3.Text);
-                }
-                if (chkbxSymptom4.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom4.Text);
-                }
-                if (chkbxSymptom5.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom5.Text);
-                }
-                if (chkbxSymptom6.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom6.Text);
-                }
-                if (chkbxSymptom7.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom7.Text);
-                }
-                if (chkbxSymptom8.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom8.Text);
-                }
-                if (chkbxSymptom9.Checked)
-                {
-                    responses.WriteLine("Experienced: " + chkbxSymptom9.Text);
-                }
-                //Health Check Question 2
-                if (chkbxyes1.Checked)
-                {
-                    responses.WriteLine("Have been in close contact with a probable or confirmed case of COVID-19");
-                }
-                else
-                {
-                    responses.WriteLine("Did not have a close contact with a probable or confirmed case of COVID-19");
-                }
-                //Health Check Question 3
-                if (chkbxyes2.Checked)
-                {
-                    responses.WriteLine("Fully Vaccinated");
-                }
-                else
-                {
-                    responses.WriteLine("Not Fully Vaccinated");
-                }
-                responses.Close();
+                month = "February";
+                File_content();
             }
+        }
+        private void File_content ()
+        {
+            StreamWriter responses = new StreamWriter(@"C:\Users\Carl Joseph\source\repos\Contact Tracing App\Responses\" + (month) + " responses.txt", true);
+            ++counter;
+            responses.WriteLine("Respondent " + counter);
+            //Personal Information Part
+            responses.WriteLine("Name: " + txtbxFirstName.Text + " " + txtbxMiddleName.Text + " " + txtbxLastName.Text);
+            responses.WriteLine("Age: " + txtbxAge.Text);
+            responses.WriteLine("Contact no.: " + txtbxcontactno.Text);
+            responses.WriteLine("Email Address: " + txtbxEmail.Text);
+            responses.WriteLine("Address: " + txtbxaddress.Text);
+            responses.WriteLine("Time of Visit: " + txtbxtime.Text);
+            responses.WriteLine("Date of Visit: " + cmboxmonth.Text + " " + cmboxDay.Text + ", " + cmboxYear.Text);
+            //Health Check question 1
+            if (chkbxSymptom1.Checked)
+            {
+                responses.WriteLine("Experienced: " + chkbxSymptom1.Text);
+            }
+            if (chkbxSymptom2.Checked)
+            {
+                responses.WriteLine("Experienced: " + chkbxSymptom2.Text);
+            }
+            if (chkbxSymptom3.Checked)
+            {
+                responses.WriteLine("Experienced: " + chkbxSymptom3.Text);
+            }
+            if (chkbxSymptom4.Checked)
+            {
+                responses.WriteLine("Experienced: " + chkbxSymptom4.Text);
+            }
+            if (chkbxSymptom5.Checked)
+            {
+                responses.WriteLine("Experienced: " + chkbxSymptom5.Text);
+            }
+            if (chkbxSymptom6.Checked)
+            {
+                responses.WriteLine("Experienced: " + chkbxSymptom6.Text);
+            }
+            if (chkbxSymptom7.Checked)
+            {
+                responses.WriteLine("Experienced: " + chkbxSymptom7.Text);
+            }
+            if (chkbxSymptom8.Checked)
+            {
+                responses.WriteLine("Experienced: " + chkbxSymptom8.Text);
+            }
+            if (chkbxSymptom9.Checked)
+            {
+                responses.WriteLine("Experienced: " + chkbxSymptom9.Text);
+            }
+            //Health Check Question 2
+            if (chkbxyes1.Checked)
+            {
+                responses.WriteLine("Have been in close contact with a probable or confirmed case of COVID-19");
+            }
+            else
+            {
+                responses.WriteLine("Did not have a close contact with a probable or confirmed case of COVID-19");
+            }
+            //Health Check Question 3
+            if (chkbxyes2.Checked)
+            {
+                responses.WriteLine("Fully Vaccinated");
+            }
+            else
+            {
+                responses.WriteLine("Not Fully Vaccinated");
+            }
+            responses.Close();
         }
     }
 }
